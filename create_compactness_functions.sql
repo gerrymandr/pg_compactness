86f1698d5a5bc33d3aca7e03d3a96e172009d596
Lee Hachadoorian <Lee.Hachadoorian@gmail.com>
2018-03-18 19:47:03 -0400

    Area-based compactness measures

--- create_compactness_functions.sql
+++ create_compactness_functions.sql
@@ -39,4 +39,36 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION reock(poly1 geometry) RETURNS double precision AS $$
+DECLARE
+  is_projected boolean;
+BEGIN
+  SELECT split_part(proj4text, ' ', 1) != '+proj=longlat' INTO is_projected
+  FROM spatial_ref_sys
+  WHERE srid = ST_SRID(poly1);
 
+  IF NOT is_projected THEN
+    RAISE EXCEPTION 'Measure not appropriate for long-lat coordinates.';
+  END IF;
+
+  RETURN ST_Area(poly1) / ST_Area(ST_MinimumBoundingCircle(ST_ConvexHull(poly1)));
+    
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION area_convex_hull(poly1 geometry) RETURNS double precision AS $$
+DECLARE
+  is_projected boolean;
+BEGIN
+  SELECT split_part(proj4text, ' ', 1) != '+proj=longlat' INTO is_projected
+  FROM spatial_ref_sys
+  WHERE srid = ST_SRID(poly1);
+
+  IF NOT is_projected THEN
+    RAISE EXCEPTION 'Measure not appropriate for long-lat coordinates.';
+  END IF;
+
+  RETURN ST_Area(poly1) / ST_Area(ST_ConvexHull(poly1));
+    
+END;
+$$ LANGUAGE plpgsql;
--- test.sql
+++ test.sql
@@ -9,3 +9,8 @@ ORDER BY 1;
 SELECT district_1, schwartzberg(geom), schwartzberg(ST_Transform(geom, 4326)::geography)
 FROM geo_cd_proposed_utm18n
 ORDER BY 1;
+
+SELECT district_1, reock(geom), area_convex_hull(geom)
+FROM geo_cd_proposed_utm18n
+ORDER BY 1;
+
