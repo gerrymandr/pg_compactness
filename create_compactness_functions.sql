1a3fe1d40fc03a62319a53f2db1090b2d4fc5f95
Lee Hachadoorian <Lee.Hachadoorian@gmail.com>
2018-03-18 19:35:19 -0400

    Shape Index compactness measures

---
+++ create_compactness_functions.sql
@@ -0,0 +1,42 @@
+﻿CREATE OR REPLACE FUNCTION polsby_popper(poly1 geometry) RETURNS double precision AS $$
+DECLARE
+  is_projected boolean;
+BEGIN
+  SELECT split_part(proj4text, ' ', 1) != '+proj=longlat' INTO is_projected
+  FROM spatial_ref_sys
+  WHERE srid = ST_SRID(poly1);
+
+  IF NOT is_projected THEN
+    RAISE EXCEPTION 'Case lat-long coordinates to geography type before calling this function.';
+  END IF;
+
+  RETURN 4 * pi() * ST_Area(poly1) / (ST_Perimeter(poly1) ^ 2);
+    
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION polsby_popper(poly1 geography) RETURNS double precision AS $$
+BEGIN
+
+  RETURN 4 * pi() * ST_Area(poly1) / (ST_Perimeter(poly1) ^ 2);
+    
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION schwartzberg(poly1 geometry) RETURNS double precision AS $$
+BEGIN
+
+  RETURN sqrt(polsby_popper(poly1));
+    
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION schwartzberg(poly1 geography) RETURNS double precision AS $$
+BEGIN
+
+  RETURN sqrt(polsby_popper(poly1));
+    
+END;
+$$ LANGUAGE plpgsql;
+
+
---
+++ test.sql
@@ -0,0 +1,11 @@
+﻿/**
+SET search_path = scratch, public;
+**/
+
+SELECT district_1, polsby_popper(geom), polsby_popper(ST_Transform(geom, 4326)::geography)
+FROM geo_cd_proposed_utm18n
+ORDER BY 1;
+
+SELECT district_1, schwartzberg(geom), schwartzberg(ST_Transform(geom, 4326)::geography)
+FROM geo_cd_proposed_utm18n
+ORDER BY 1;
